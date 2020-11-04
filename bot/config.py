249dad25604463e5d51f3ed4e47848e701611bea
martyushevleoned <56407820+martyushevleoned@users.noreply.github.com>
2020-11-05 01:19:21 +0500

    first add

---
+++ bot/config.py
@@ -0,0 +1,159 @@
+from telebot import types
+from datetime import datetime
+
+TOKEN = '1384356348:AAG-nT3EslVaMkrAZIMMB66qXqW6U8ovYqA'
+EDITORS = [976798046]
+BLACK_LIST = [470540326]
+
+
+class DataBase:
+    # subjects
+    sub = [
+        'обж',  # 0
+        'химия',  # 1
+        'физика',  # 2
+        'алгебра',  # 3
+        'история',  # 4
+        'геометрия',  # 5
+        'экономика',  # 6
+        'география',  # 7
+        'литература',  # 8
+        'информатика',  # 9
+        'физкультура',  # 10
+        'правоведение',  # 11
+        'русский язык',  # 12
+        'обществознание',  # 13
+        'иностранный язык',  # 14
+        'практикум по математике'  # 15
+    ]
+
+    # homework
+    hw = dict.fromkeys(sub, '-')
+
+    # schedule
+    schedule = {
+        'понедельник': [sub[0],
+                        sub[14],
+                        sub[3],
+                        sub[3],
+                        sub[8],
+                        sub[6],
+                        sub[11]],
+
+        'вторник': [sub[8],
+                    sub[15],
+                    sub[8],
+                    sub[1],
+                    sub[2],
+                    sub[12],
+                    sub[10]],
+
+        'среда': [sub[2],
+                  sub[2],
+                  sub[14],
+                  sub[12],
+                  sub[12],
+                  sub[9]],
+
+        'четверг': [sub[13],
+                    sub[13],
+                    sub[2],
+                    sub[10],
+                    sub[7],
+                    sub[5],
+                    sub[5]],
+
+        'пятница': [sub[9],
+                    sub[9],
+                    sub[3],
+                    sub[3],
+                    sub[4],
+                    sub[2],
+                    sub[15]],
+
+        'суббота': [sub[14],
+                    sub[9],
+                    sub[4],
+                    sub[8]]
+    }
+
+    # timetable
+    timetable = ['1) 8:15 - 8:55',
+                 '2) 9:15 - 9:55',
+                 '3) 10:15 - 10:55',
+                 '4) 11:15 - 11:55',
+                 '5) 12:15 - 12:55',
+                 '6) 13:00 - 13:40',
+                 '7) 13:45 - 14:25']
+
+    # methods
+    def timetable_out(self):
+        answer = ''
+        for i in range(len(self.timetable)):
+            answer += self.timetable[i]
+            answer += '\n'
+        return answer
+
+    def schedule_out(self):
+        answer = ''
+        for k, v in self.schedule.items():
+            answer += k.capitalize() + '\n'
+            count = 1
+            for i in v:
+                answer += f'{count}) {i.capitalize()} \n'
+                count += 1
+            answer += '\n'
+        return answer
+
+    def tomorrow_hw_out(self):
+        return self.day_hw_out(datetime.today().isoweekday())
+
+    def all_hw_out(self):
+        answer = ''
+        for k, v in self.hw.items():
+            answer += k.capitalize() + '\n'
+            answer += v + '\n' + '\n'
+        return answer
+
+    def day_hw_out(self, date):
+        date %= 7
+        if date == 6:
+            date = 0
+        answer = list(self.schedule.keys())[date].capitalize()
+        answer += '\n' + '\n'
+        count = 1
+        for i in self.schedule[list(self.schedule.keys())[date]]:
+            answer += f'{count}) {i.capitalize()}\n'
+            answer += self.hw[i] + '\n' + '\n'
+            count += 1
+        return answer
+
+    def save(self):
+        with open('data.txt', 'w') as file:
+            for v in self.hw.values():
+                file.write(str(v).replace('\n', ' <3 ') + '\n')
+            file.close()
+
+    def load(self):
+        with open('data.txt', 'r') as file:
+            for i in self.sub:
+                self.hw[i] = file.readline().replace('\n', '')
+                self.hw[i] = self.hw[i].replace(' <3 ', '\n')
+            file.close()
+
+
+func = DataBase()
+
+day_key = types.InlineKeyboardMarkup()
+day_key.add(types.InlineKeyboardButton(text='понедельник', callback_data='0'))
+day_key.add(types.InlineKeyboardButton(text='вторник', callback_data='1'))
+day_key.add(types.InlineKeyboardButton(text='среда', callback_data='2'))
+day_key.add(types.InlineKeyboardButton(text='четверг', callback_data='3'))
+day_key.add(types.InlineKeyboardButton(text='пятница', callback_data='4'))
+day_key.add(types.InlineKeyboardButton(text='суббота', callback_data='5'))
+
+# keyboards
+sub_key = types.InlineKeyboardMarkup()
+for i in func.sub:
+    sub_key.add(types.InlineKeyboardButton(text=i, callback_data=i))
+sub_key.add(types.InlineKeyboardButton(text='выход', callback_data='exit'))
---
+++ bot/main.py
@@ -0,0 +1,133 @@
+import telebot  # pip3 install PyTelegramBotAPI
+from config import TOKEN, EDITORS, BLACK_LIST, func, sub_key, day_key
+
+messages = {
+    '/timetable': 'func.timetable_out()',
+    '/schedule': 'func.schedule_out()',
+    '/tomorrow': 'func.tomorrow_hw_out()',
+    '/all_hw': 'func.all_hw_out()',
+}
+
+commands = {
+    '/day_hw': 'bot.send_message(m.chat.id, "выберите день", reply_markup=day_key)'
+}
+
+debug = {
+    '/reset': 'reset()'
+}
+
+current_chat = 0
+current_lesson = ''
+waiter = False
+
+
+def reset():
+    global current_chat, waiter, current_lesson
+    current_chat = 0
+    current_lesson = ''
+    waiter = False
+
+
+bot = telebot.TeleBot(TOKEN)
+
+
+def listener(m):
+    for msg in m:
+        if msg.content_type == 'text':
+            with open('messages.txt', 'a') as mess:
+                mess.write(
+                    str(msg.chat.id) + ' ' + msg.chat.first_name + ': ' + msg.text.replace('\n', ' <3 ') + '\n')
+                mess.close()
+
+
+bot.set_update_listener(listener)
+
+
+@bot.message_handler(commands=['add_hw'])
+def start_message(m):
+    global current_chat
+    if m.chat.id in EDITORS:
+        if current_chat == 0:
+            current_chat = m.chat.id
+            bot.send_message(m.chat.id, 'выберите предмет', reply_markup=sub_key)
+        else:
+            bot.send_message(m.chat.id, 'подожди пару минут')
+    else:
+        bot.send_message(m.chat.id, 'недостаточно прав доступа')
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def handler_call(call):
+    # day hw
+    for i in range(6):
+        if call.data == str(i):
+            bot.edit_message_text(
+                chat_id=call.message.chat.id,
+                message_id=call.message.message_id,
+                text=func.day_hw_out(i),
+                reply_markup=None,
+                parse_mode='Markdown')
+
+    # exit from admin mod
+    if call.data == 'exit':
+        reset()
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            text='выход',
+            reply_markup=None,
+            parse_mode='Markdown')
+
+    # choose subject
+    elif call.data in func.sub:
+
+        global current_chat, current_lesson, waiter
+
+        if call.message.chat.id == current_chat:
+            current_lesson = call.data
+            waiter = True
+            bot.edit_message_text(
+                chat_id=call.message.chat.id,
+                message_id=call.message.message_id,
+                text=call.data + '\n' + 'текущее дз:' + '\n' + func.hw[current_lesson] + '\nвведите дз:',
+                reply_markup=None,
+                parse_mode='Markdown')
+
+        # protect
+        else:
+            bot.edit_message_text(
+                chat_id=call.message.chat.id,
+                message_id=call.message.message_id,
+                text='nope',
+                reply_markup=None,
+                parse_mode='Markdown')
+
+
+# text
+@bot.message_handler(content_types=['text'])
+def send_text(m):
+    if m.chat.id in BLACK_LIST:
+        bot.send_message(m.chat.id, 'тебе здесь не рады')
+
+    elif m.chat.id == current_chat and waiter is True:
+        func.hw[current_lesson] = m.text
+        func.save()
+        bot.send_message(m.chat.id, 'дз сохранено')
+        reset()
+
+    elif m.text in messages.keys():
+        bot.send_message(m.chat.id, eval(messages[m.text]))
+
+    elif m.text in commands:
+        eval(commands[m.text])
+
+    elif m.text in debug and m.chat.id in EDITORS:
+        eval(debug[m.text])
+        bot.send_message(m.chat.id, 'что бы ты ни делал, я это сделал')
+
+    else:
+        bot.send_message(m.chat.id, 'я не понял, что ты написал')
+
+
+func.load()
+bot.polling(none_stop=True)
